7899a467108284363a0737a2e350606fe4e3d766
goas9001 <ashish.gorai.consultant@nielsen.com>
2019-11-23 00:42:23 +0530

    animation bug fixed

--- src/app/views/home/components/all-faqs/all-faqs.component.ts
+++ src/app/views/home/components/all-faqs/all-faqs.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedAnimations } from "src/app/shared/animations/shared-animations";
 
 @Component({
   selector: 'app-all-faqs',
   templateUrl: './all-faqs.component.html',
-  styleUrls: ['./all-faqs.component.scss']
+  styleUrls: ['./all-faqs.component.scss'],
+  animations: [SharedAnimations]
 })
 export class AllFaqsComponent implements OnInit {
 
